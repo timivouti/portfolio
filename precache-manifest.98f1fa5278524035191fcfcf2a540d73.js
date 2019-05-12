8ef767de17f6cfd358eec12c91b991a0ce150c01
Timi Voutilainen <voutilainen.timi@gmail.com>
2019-05-13 00:43:30 +0300

    Updates

---
+++ precache-manifest.98f1fa5278524035191fcfcf2a540d73.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07d497f61cf65c4b79d92f8819f5d182",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "28af3b4b0b56363ed477",
+    "url": "/portfolio/static/css/main.7ee6cee8.chunk.css"
+  },
+  {
+    "revision": "d71bad7d6f7f1ee4e0ca",
+    "url": "/portfolio/static/js/2.644a98c3.chunk.js"
+  },
+  {
+    "revision": "28af3b4b0b56363ed477",
+    "url": "/portfolio/static/js/main.c08670bf.chunk.js"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  }
+]);
